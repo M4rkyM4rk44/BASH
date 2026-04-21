e6e137c7d6e7a4d530fb1aaf182b05e98508a109
Mark <197963436+M4rkyM4rk44@users.noreply.github.com>
2026-04-21 08:26:55 -0400

    Update Check-Password.sh

--- Check-Password.sh
+++ Check-Password.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-# Script
+# Get User Password
 read -p "Enter password to check: " PWD
 HASH=$(echo -n $PWD | sha1sum | awk '{print $1}')
+
+# Check Password
 RESULT=$(curl -s https://api.pwnedpasswords.com/range/$(echo $HASH | cut -c1-5) | grep -i ${HASH:5} || echo "0:0")
+
+# Display Results
 echo "Your password has leaked $(echo $RESULT | awk -F: '{print $2}' | sed 's/[^0-9]//g') times."
