6d84d3be7b2d2005bae9d65b32b656be37eab626
Mark <197963436+M4rkyM4rk44@users.noreply.github.com>
2026-04-21 08:39:01 -0400

    Update backup script to use dynamic username

--- Backup-Proton-Emails.sh
+++ Backup-Proton-Emails.sh
@@ -4,14 +4,14 @@
 DATE=$(date +%Y"-"%m"-"%d) # Todays Date
 A="/home/$USER/proton-mail-export-cli" # Proton Mail Export Tool Location
 O="backup" # Export Tool Option to Use
-D="/home/$USER/Cabinet/98_Resources/02_Backups/09_Emails/" # Backup Location
+D="/home/$USER/" # Backup Location
 U="user@proton.me" # Put Your Protonmail Username Here
 
 # Create backup
 $A --operation $O --dir $D --user $U --telemetry
-tar -cJvf "Proton-Mail-Backup_"$DATE.tar.xz -C ./ markgkaitting@proton.me
+tar -cJvf "Proton-Mail-Backup_"$DATE.tar.xz -C ./ $U
 md5sum "Proton-Mail-Backup_"$DATE.tar.xz > "Proton-Mail-Backup_"$DATE.tar.xz.md5
-rm -fr markgkaitting@proton.me
+rm -fr $U
 echo ""
 read -p "Emails exported, archived, and MD5 checksum generated..."
 echo ""
