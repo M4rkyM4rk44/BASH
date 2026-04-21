c48cd469f45ecdfee3fea2bbc0a4b856be082b57
Mark <197963436+M4rkyM4rk44@users.noreply.github.com>
2026-04-21 08:22:19 -0400

    Refactor Install-Brave.sh for clarity and structure

---
+++ Install-Brave.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# This script is for installing Brave Web Browser 
+# on Debian based (Specificly Ubuntu Based) Linux
+# operating systems. This script was created for 
+# Linux Mint but should work on any Debian based OS.
+
+while true
+do
+  # Menu
+  clear
+	echo "Select an option:"
+	echo ""
+	echo "0) Exit Script"
+	echo "1) Install Brave"
+	echo "2) UnInstall Brave"
+	echo ""
+	read -p "Option: " n
+  case $n in
+		0)
+			clear
+			exit
+			;;
+		1) 
+			# Install Brave
+      clear
+			sudo apt install curl -y
+      sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+      echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=$( dpkg --print-architecture )] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+      sudo apt update
+      sudo apt install brave-browser
+			echo ""
+			echo "-----Brave Installed-----"
+			echo ""
+			read -p "Press any key to close terminal ..."
+			break
+			;;
+		2) 
+      # UnInstall Brave
+			clear
+			sudo apt purge brave-browser -y
+			echo ""
+			echo "-----Brave UnInstalled-----"
+			echo ""
+			read -p "Press any key to close terminal ..."
+			break
+			;;
+		*) 
+      # Invalid Option
+			clear
+			echo ""
+			echo "Invalid Option!!"
+			echo ""
+			read -p "Press any key to return to menu ..."
+			;;
+	esac
+done
+clear
